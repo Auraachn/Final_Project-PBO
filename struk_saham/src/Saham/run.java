85faaaff7e818718d0a00af21636e2667dfa289f
Aura Choirun Nisa <120537279+Auraachn@users.noreply.github.com>
2023-06-13 10:35:35 +0700

    Add files via upload

---
+++ struk_saham/src/Saham/run.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Saham;
+
+/**
+ *
+ * @author AURA
+ */
+public class run {
+    public static void main(String args[]) {
+        Login_page login = new Login_page();
+        login.setVisible(true);
+        login.pack();
+    }
+}
